6eaae0f754753a9dd89ed4b00c3b9e59d2dc651d
Dowglas Maia <lucio.maia@ilog.com.br>
2022-04-24 17:49:01 -0300

    impl middleware personalizado de Error

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/app-error';
+
 
 const app = express();
 
@@ -8,7 +10,26 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
-//cont
+
+app.use(
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    });
+
+  });
 
 app.listen(3333, () => {
   console.log('Server staterd on port 3333!');
