77245ab6858dbdf16924a3252a7add6306e720ea
Dowglas Maia <dowglasmaia@gmail.com>
2022-05-07 10:22:35 -0300

    Criando Service listar Todos

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import { getCustomRepository } from 'typeorm';
+import Product from '../type-orm/entities/Product';
+import { ProductsRepository } from '../type-orm/repositories/ProductsRepository';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
