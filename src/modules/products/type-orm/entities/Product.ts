77d1aa41e59e49945f8f4f25eb74a4e4476c90db
Dowglas Maia <dowglasmaia@gmail.com>
2022-05-07 10:11:57 -0300

    Criando Service para criar produto

--- src/modules/products/type-orm/entities/Product.ts
+++ src/modules/products/type-orm/entities/Product.ts
@@ -1,33 +1,29 @@
 import {
-	Column,
-	CreateDateColumn,
-	Entity,
-	PrimaryGeneratedColumn,
-	UpdateDateColumn,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('product')
 class Product {
-	@PrimaryGeneratedColumn('uuid')
-	id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-	@Column({
-		type: 'varchar',
-		length: 100,
-		unique: true,
-	})
-	name: string;
+  @Column({ type: 'varchar', length: 100, unique: true })
+  name: string;
 
-	@Column('decimal')
-	pice: number;
+  @Column('decimal')
+  price: number;
 
-	@Column('int')
-	quantity: number;
+  @Column('int')
+  quantity: number;
 
-	@CreateDateColumn()
-	created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-	@UpdateDateColumn()
-	updated_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 export default Product;
